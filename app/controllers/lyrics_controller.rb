c0a8601644cbd469a9f3a3075d0ae0f0688a6567
CC Evans <ccevans1@gmail.com>
2014-12-20 15:07:17 -0500

    Add and setup voting

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,10 +1,9 @@
 class LyricsController < ApplicationController
-	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@lyrics = Lyric.all.order("created_at DESC")
-		@comments = Comment.where(lyric_id: @lyric)
 	end
 
 	def show
@@ -42,6 +41,17 @@ class LyricsController < ApplicationController
 		@lyric.destroy
 		redirect_to root_path
 	end
+
+	def upvote
+		@lyric.upvote_by current_user
+		redirect_to :back
+	end
+
+	def downvote
+		@lyric.downvote_by current_user
+		redirect_to :back
+	end
+
 
 	private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :lyrics do
+  	member do
+  		get "like", to: "lyrics#upvote"
+  		get "dislike", to: "lyrics#downvote"
+  	end
+
   	resources :comments
   end
   
