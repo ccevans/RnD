3514d550d862a028af8e655fa87018f24407f6a2
CC Evans <ccevans1@gmail.com>
2015-03-01 22:23:04 -0500

    updated new lyric and art pages

--- app/models/art.rb
+++ app/models/art.rb
@@ -11,6 +11,6 @@ class Art < ActiveRecord::Base
 	scope :by_tags, -> tags { where(:tags => tags) }
 	
 
-	has_attached_file :image, :styles => { :medium => "500x500#", :small => "300x300#" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :large => "1200x1200#", :medium => "500x500#", :small => "300x300#" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
 
 
 
-	 TYPEOFS = %w[video audio picture]
+	 TYPEOFS = %w[picture video audio]
 
 	has_attached_file :image, :styles => { :large => "600x600#", :medium => "400x400#", :small => "200x200#" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
