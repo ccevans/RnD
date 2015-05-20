575a16ce41368d05c35d6a7e7356ee986cb68826
CC Evans <ccevans1@gmail.com>
2015-05-19 22:18:36 -0400

    adding homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,12 @@ class PagesController < ApplicationController
   def launch
 
   end
+
+  def home
+  	@home_lyric = Lyric.all.order("RANDOM()").first
+  	@home_campaign = Campaign.open.all.order("created_at DESC").last
+
+
+  end
+
 end
