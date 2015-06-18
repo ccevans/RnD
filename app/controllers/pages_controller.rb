98858626d52115cc7151fa0091e109a627a655ae
CC Evans <ccevans1@gmail.com>
2015-06-18 09:11:58 -0400

    featured profile fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
   def home
   	@home_lyric = Lyric.all.order("RANDOM()").first
   	@home_campaign = Campaign.open.all.order("created_at ASC").last
-  	@featured_users = User.featured.all.order("created_at DESC")
+  	@featured_users = User.featured.all.order("created_at DESC").take(2)
 
 
   end
