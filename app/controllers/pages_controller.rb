2ac23e1f9a72265f02db499a016838c4cd510ccd
CC Evans <ccevans1@gmail.com>
2015-07-03 14:22:24 -0400

    updates on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
   	@home_lyric = Lyric.all.order("RANDOM()").first
   	@home_campaign = Campaign.open.all.order("created_at ASC").last
   	@featured_users = User.featured.all.order("created_at DESC").take(2)
-  	@home_posts = Post.all.order("created_at DESC")
+  	@home_posts = Post.all.order("created_at DESC").take(4)
 
   end
 
