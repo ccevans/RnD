3b7a5673f750fd20ace180850aab181fbeac26f2
CC Evans <ccevans1@gmail.com>
2015-07-22 09:40:58 -0400

    updates of home page

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -9,7 +9,7 @@ class Campaign < ActiveRecord::Base
 	scope :closed, -> { where(:status => "closed") }
 	scope :draft, -> { where(:status => "draft") }
 
-	STATUS = %w[draft free open closed]
+	STATUS = %w[draft free open closed hide]
 
 
 	has_attached_file :image, :styles => { :large => "1200x300#", :medium => "800x200#", :thumb => "80x20#"  }, :default_url => "/images/:style/profile-missing.png"  
